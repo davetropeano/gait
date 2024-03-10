95312cd46573cd41174615c9b399646bd1786a94
davetropeano <davetropeano@gmail.com>
2024-03-10 00:35:22 -0500

    refactored to use dataclass

--- gait_analysis_davet.py
+++ gait_analysis_davet.py
@@ -5,10 +5,15 @@ Created on Tue Feb 13 16:29:52 2024
 @author: lexie
 
 NOTES
-- using numpy for as much math as possible. Or do without numpy
 - you had multiple versions of calculating a discrete derivative. The code was identical except for the variable names. Simplified to one function d_dt
 - AVOID using hard coded numbers when there are enumerated values to use instead
+- removed code that looked like it wasn't being used
+- used a dataclass to reduce amount of repetitive code
+- csv output everything in a regular fashion (even data you don't need)
+- increased font size and spacing on video output
 """
+
+from dataclasses import dataclass
 
 import csv
 import numpy as np
@@ -18,6 +23,8 @@ import mediapipe as mp
 # Initialize MediaPipe Pose
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
+
+# ------------------
 
 # calculate Euclidean distance between two points
 def distance(point1, point2):
@@ -45,7 +52,8 @@ def d_dt(initial, final, dt):
     
     return (final - initial) / dt
 
-# Open a video file
+# -----------------------
+
 video_path = "videos/TS1fl.mov"
 cap = cv2.VideoCapture(video_path)
 
@@ -62,44 +70,47 @@ output_video = cv2.VideoWriter('output_video_p22.mp4', fourcc, fps, (width, heig
 # Create and open a CSV file for writing
 csv_file = open('pose_measurements_p22.csv', 'w', newline='')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Frame', 'Foot Magnitude', 'Shank Magnitude', 'Thigh Magnitude', 'Trunk Magnitude', 'Ankle Plantar/Dorsi-Flexion Angle', 'Knee Flexion/Extension Angle', 'Hip Flexion/Extension'])
+csv_writer.writerow([
+     'Frame', 
+     'Foot Magnitude', 'Shank Magnitude', 'Thigh Magnitude', 'Trunk Magnitude', 
+     'Toe Angle', 'Ankle Angle', 'Knee Angle', 'Hip Angle',
+     'Toe Linear Velocity', 'Ankle Linear Velocity', 'Knee Linear Velocity', 'Hip Linear Velocity',
+     'Toe Angular Velocity', 'Ankle Angular Velocity', 'Knee Angular Velocity', 'Hip Angular Velocity',
+     'Toe Angular Acceleration', 'Ankle Angular Acceleration', 'Knee Angular Acceleration', 'Hip Angular Acceleration'
+])
+
+# -----------------------
+
+@dataclass
+class LandmarkData():
+    curr: any = None
+    prev: any = None
+    angle: float = None
+    prev_angle: float = None
+    linear_velocity: float = 0.0
+    angular_velocity: float = None
+    prev_angular_velocity: float = None
+    angular_acceleration: float = 0.0
+
+    def update(self, landmark, angle):
+        self.prev = self.curr
+        self.prev_angle = self.angle
+        self.prev_angular_velocity = self.angular_velocity
+
+        self.curr = landmark
+        self.angle = angle
+        if self.prev is not None:                
+            self.linear_velocity = d_dt(self.prev.x, self.curr.x, dt)               
+            self.angular_velocity = d_dt(self.prev_angle, self.angle, dt)
+            if self.prev_angular_velocity is not None:
+                self.angular_acceleration = d_dt(self.prev_angular_velocity, self.angular_velocity, dt)
+                
+toe = LandmarkData()
+ankle = LandmarkData()
+knee = LandmarkData()
+hip = LandmarkData()
 
 frame_count = 0
-ankle_angles = []           
-knee_angles = []            
-hip_angles = []            
-
-hip_lin_velocities = []     
-knee_lin_velocities = []   
-ankle_lin_velocities = []   
-toe_lin_velocities = []   
-
-hip_ang_velocities = []   
-knee_ang_velocities = []  
-ankle_ang_velocities = []   
-
-hip_ang_accelerations = []  
-knee_ang_accelerations = []  
-ankle_ang_accelerations = []  
-
-prev_toe_landmark = None      
-prev_ankle_landmark = None    
-prev_knee_landmark = None     
-prev_hip_landmark = None      
-
-linear_velocity_hip = None    
-linear_velocity_knee = None   
-linear_velocity_ankle = None  
-linear_velocity_toe = None    
-
-angular_velocity_hip = None   
-angular_velocity_knee = None  
-angular_velocity_ankle = None 
-
-angular_accel_hip = None      
-angular_accel_knee = None     
-angular_accel_ankle = None    
-
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -122,76 +133,20 @@ while cap.isOpened():
         shank_magnitude = distance(knee_landmark, ankle_landmark)
         thigh_magnitude = distance(hip_landmark, knee_landmark)
         trunk_magnitude = distance(shoulder_landmark, hip_landmark)
-        
-        # Calculate Segment Angles 
-        ankle_angle = angle(knee_landmark, ankle_landmark, toe_landmark)
-        knee_angle = angle(hip_landmark, knee_landmark, ankle_landmark) 
-        hip_angle = angle(knee_landmark, hip_landmark, shoulder_landmark)
 
-    
-        hip_angles.append(hip_angle)
-        knee_angles.append(knee_angle)
-        ankle_angles.append(ankle_angle)
-
-        # Hip Data Collection
-        if prev_hip_landmark is not None:                
-            linear_velocity_hip = d_dt(prev_hip_landmark.x, hip_landmark.x, dt) 
-            hip_lin_velocities.append(linear_velocity_hip)
-                
-            angular_velocity_hip = d_dt(prev_hip_angle, hip_angle, dt) 
-            hip_ang_velocities.append(angular_velocity_hip)
-        
-            if len(hip_ang_velocities) >= 2:
-                # davet - this seems wrong. Why index by [-2]? Don't you just want the previous value?
-                angular_accel_hip = d_dt(hip_ang_velocities[-2], angular_velocity_hip, dt) 
-                hip_ang_accelerations.append(angular_accel_hip)
-                
-        prev_hip_landmark = hip_landmark
-        prev_hip_angle = hip_angle
-        
-        # Knee Data Collection
-        if prev_knee_landmark is not None:
-            linear_velocity_knee = d_dt(prev_knee_landmark.x, knee_landmark.x, dt)
-            knee_lin_velocities.append(linear_velocity_knee)
-                
-            angular_velocity_knee = d_dt(prev_knee_angle, knee_angle, dt)
-            knee_ang_velocities.append(angular_velocity_knee)
-                
-            if len(knee_ang_velocities) >= 2:
-                # davet - this seems wrong. Why index by [-2]? Don't you just want the previous value?
-                angular_accel_knee = d_dt(knee_ang_velocities[-2], angular_velocity_knee, dt)
-                knee_ang_accelerations.append(angular_accel_knee)
-                
-        prev_knee_landmark = knee_landmark
-        prev_knee_angle = knee_angle
-                
-        # Ankle Data Collection                
-        if prev_ankle_landmark is not None:
-            linear_velocity_ankle = d_dt(prev_ankle_landmark.x, ankle_landmark.x, dt)
-            ankle_lin_velocities.append(linear_velocity_ankle)
-                
-            angular_velocity_ankle = d_dt(prev_ankle_angle, ankle_angle, dt)
-            ankle_ang_velocities.append(angular_velocity_ankle)
-                
-            if len(ankle_ang_velocities) >= 2:
-                # davet - this seems wrong. Why index by [-2]? Don't you just want the previous value?
-                angular_accel_ankle = d_dt(ankle_ang_velocities[-2], angular_velocity_ankle, dt)
-                ankle_ang_accelerations.append(angular_accel_ankle)
-
-        prev_ankle_landmark = ankle_landmark
-        prev_ankle_angle = ankle_angle
+        toe.update(toe_landmark, 0.0)
+        hip.update(hip_landmark, angle(knee_landmark, hip_landmark, shoulder_landmark))
+        knee.update(knee_landmark, angle(hip_landmark, knee_landmark, ankle_landmark))
+        ankle.update(ankle_landmark, angle(knee_landmark, ankle_landmark, toe_landmark))        
           
-        # Toe Data Collection                
-        if prev_toe_landmark is not None:
-            linear_velocity_toe = d_dt(prev_toe_landmark.x, toe_landmark.x, dt)
-            toe_lin_velocities.append(linear_velocity_toe)
-        
-        prev_toe_landmark = toe_landmark
-
-        
-        csv_writer.writerow([frame_count, foot_magnitude, shank_magnitude, thigh_magnitude, trunk_magnitude, None, None, ankle_angle, knee_angle, hip_angle, 
-                             None, None, linear_velocity_hip, linear_velocity_knee, linear_velocity_toe, angular_velocity_hip, angular_velocity_knee, 
-                             angular_velocity_ankle, None, None, angular_accel_hip, angular_accel_knee, angular_accel_ankle])
+        csv_writer.writerow([
+            frame_count, 
+            foot_magnitude, shank_magnitude, thigh_magnitude, trunk_magnitude, 
+            toe.angle, ankle.angle, knee.angle, hip.angle,
+            toe.linear_velocity, ankle.linear_velocity, knee.linear_velocity, hip.linear_velocity,
+            toe.angular_velocity, ankle.angular_velocity, knee.angular_velocity, hip.angular_velocity,
+            toe.angular_acceleration, ankle.angular_acceleration, knee.angular_acceleration, hip.angular_acceleration,
+        ])
 
         
         
@@ -206,8 +161,8 @@ while cap.isOpened():
 
         # Display live measurements on the frame with color-coded font
         font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 0.5
-        font_thickness = 1
+        font_scale = 1.0
+        font_thickness = 3
 
         # Define colors corresponding to each measurement
         color_hip_angle = (255, 0, 0)  # Red
@@ -217,14 +172,17 @@ while cap.isOpened():
         color_shank_magnitude = (0, 255, 0)  # Green
         color_foot_magnitude = (0, 255, 0)  # Green
 
+        # davet - making all the text black seems more readable to me
+        color_hip_angle = color_knee_angle = color_ankle_angle = color_thigh_magnitude = color_shank_magnitude = color_foot_magnitude = (0,0,0) # black
+
 
         # Display live measurements on the frame
         cv2.putText(frame, f'Foot Length: {foot_magnitude:.2f}', (10, 30), font, font_scale, color_foot_magnitude, font_thickness)
-        cv2.putText(frame, f'Shank Length: {shank_magnitude:.2f}', (10, 120), font, font_scale, color_shank_magnitude, font_thickness)
-        cv2.putText(frame, f'Thigh Length: {thigh_magnitude:.2f}', (10, 120), font, font_scale, color_thigh_magnitude, font_thickness)
-        cv2.putText(frame, f'Knee Joint Angle: {knee_angle:.2f} degrees', (10, 270), font, font_scale, color_knee_angle, font_thickness)
-        cv2.putText(frame, f'Hip Joint Angle: {hip_angle:.2f} degrees', (10, 60), font, font_scale, color_hip_angle, font_thickness)
-        cv2.putText(frame, f'Ankle Joint Angle: {ankle_angle:.2f} degrees', (10, 90), font, font_scale, color_ankle_angle, font_thickness)
+        cv2.putText(frame, f'Shank Length: {shank_magnitude:.2f}', (10, 60), font, font_scale, color_shank_magnitude, font_thickness)
+        cv2.putText(frame, f'Thigh Length: {thigh_magnitude:.2f}', (10, 90), font, font_scale, color_thigh_magnitude, font_thickness)
+        cv2.putText(frame, f'Knee Joint Angle: {knee.angle:.2f} degrees', (10, 120), font, font_scale, color_knee_angle, font_thickness)
+        cv2.putText(frame, f'Hip Joint Angle: {hip.angle:.2f} degrees', (10, 150), font, font_scale, color_hip_angle, font_thickness)
+        cv2.putText(frame, f'Ankle Joint Angle: {ankle.angle:.2f} degrees', (10, 180), font, font_scale, color_ankle_angle, font_thickness)
 
 
     # Write frame to the output video
@@ -236,12 +194,6 @@ while cap.isOpened():
         break
 
     frame_count += 1
-
-# Calculate the difference between the max and min values of hip hinge and ankle angles
-max_hip_angle = max(hip_angles) 
-max_knee_angle = max(knee_angles)
-max_ankle_angle = max(ankle_angles) 
-
 
 # Release resources
 cap.release()
